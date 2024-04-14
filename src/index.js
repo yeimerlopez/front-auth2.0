9c836f1911ec01068e893398e4c53fd7f39149a9
yeimer lopez <yeimerlopez1216@gmail.com>
2024-04-13 22:32:56 -0500

    proyecto funcional hasta la logIn && logout

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';// se importa  para el Auth0
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain="dev-pc7vsv8tpuyurytj.us.auth0.com"
+    clientId="Py4ieqb8UfUbIKKvuwVagKlo4FOAqzLJ"
+    authorizationParams={{
+    redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,  
   </React.StrictMode>
 );
 
